abd66a5bf4da2717565a967a5870d16cd663adce
harryxu <harryzhxu@gmail.com>
2010-01-12 15:06:51 +0800

    init files

---
+++ dnswh/Swher.py
@@ -0,0 +1,42 @@
+# encoding=utf-8
+
+import sys
+
+class SwherBase(object):
+    def getCurrentDNS(self):
+        """docstring for getCurrentDNS"""
+        pass
+    
+    def setDNS(self, dns):
+        """docstring for setDNS"""
+        pass
+
+    def useInterface(self):
+        """docstring for userInterface"""
+        pass
+
+class WinSwher(SwherBase):
+    def __init__(self):
+        c = wmi.WMI()
+        interfaces = c.Win32_NetworkAdapterConfiguration(IPEnabled=1)
+
+        if len(interfaces) > 0:
+            self.interfae = interfaces[0]
+
+    def getDNS(self):
+        if not self.interfae:
+            raise Exception('no netword interfaces found')
+
+        return self.interfae.DNSServerSearchOrder[0]
+    
+    def setDNS(self, dns):
+        pass
+
+########################
+        
+os = sys.platform
+
+if os == 'win32':
+    import wmi
+    Swher = WinSwher
+
---
+++ example.py
@@ -0,0 +1,6 @@
+# encoding=utf-8
+
+from dnswh import Swher
+
+s = Swher.Swher()
+print s.getDNS()
